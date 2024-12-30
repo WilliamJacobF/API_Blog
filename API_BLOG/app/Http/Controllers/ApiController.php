3b5ffb269ccadca9c13b83118aa21826bd38522f
WilliamJacobF <155196664+WilliamJacobF@users.noreply.github.com>
2024-12-29 21:23:33 -0300

    adiconandovalidacao

--- API_BLOG/app/Http/Controllers/ApiController.php
+++ API_BLOG/app/Http/Controllers/ApiController.php
@@ -12,14 +12,22 @@ class ApiController extends Controller
         return Api::all();
     }
     public function store(Request $request){
-        Api::create($request->all());
+        $validated = $request->validate([
+            'titulo' => 'required|string|max:200',
+            'texto' => 'required|string',
+        ]);
+        Api::create($validated);
     }
     public function show($id){
         return Api::findOrfail($id);
     }
     public function update(Request $request, $id){
+        $validated = $request->validate([
+            'titulo' => 'required|string|max:200',
+            'texto' => 'required|string',
+        ]);
         $blog = Api::findOrFail($id);
-        $blog->update($request->all());
+        $blog->update($validated);
     }
     public function destroy($id){
         $blog = Api::findOrFail($id);
